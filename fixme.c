3a73d2e91c4639af1b881d2bb65a1b5b0999475c
Luna_Seline <lunamoon.seline@gmail.com>
2025-03-18 21:34:37 +0530

    Fixed the bugs

--- fixme.c
+++ fixme.c
@@ -2,15 +2,17 @@
 
 int fibonacci(int n) {
     if (n <= 0) return 0;
-    if (n == 1) return 1; //Fix me
+    if (n == 1) return 0;
+    if (n == 2) return 1;//added n==2 condition
     
     int a = 0, b = 1, temp;
     for (int i = 2; i <= n; i++) {
         temp = a + b;
         a = b;
         b = temp;
-        return b; //Fix me
+         //sent return b out of for loop
     }
+    return b;
 }
 
 int main() {
