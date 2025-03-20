af79ffdbf8c2ba1f901df46f4c94e2b3d215eb7b
vanshikadureja <vanshikadureja8@gmail.com>
2025-03-20 20:29:10 +0530

    Modified fixme.java

--- fixme.java
+++ fixme.java
@@ -1,11 +1,9 @@
 public class FibonacciChallenge {
     
     public static void main(String[] args) {
-        // Fix me: Should we allow negative numbers as input?
         System.out.println("Fibonacci(5) = " + fibonacci(5));
         System.out.println("Fibonacci(10) = " + fibonacci(10));
         
-        // Fix me: What happens with this test case?
         System.out.println("First 8 Fibonacci numbers:");
         printFibonacciSequence(8);
         
@@ -14,34 +12,40 @@ public class FibonacciChallenge {
     }
     
     public static int fibonacci(int n) {
-        // Fix me: Remember, Fibonacci sequence starts with 0, 1, ...
-        
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
     
     public static void printFibonacciSequence(int n) {
-        // Fix me: Off-by-one error?
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             System.out.print(fibonacci(i) + " ");
         }
         System.out.println();
     }
     
     public static int efficientFibonacci(int n) {
-        // Fix me: How can you avoid recalculating the same Fibonacci numbers repeatedly?
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        
         int a = 0;
         int b = 1;
         
-        if (n == 0) {
-            return a;
-        }
-        
-        for (int i = 2; i < n; i++) {
-            int temp = a;
+        for (int i = 2; i <= n; i++) {
+            int temp = a + b;
             a = b;
-            b = temp + a;
+            b = temp;
         }
         
         return b;
     }
 }
+
